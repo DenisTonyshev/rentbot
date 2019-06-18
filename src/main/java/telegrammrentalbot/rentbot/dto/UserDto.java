4a1c72346004c763e3a7f8032ac2c76ce66beeae
RomaE <ere.roma@gmail.com>
2019-06-18 13:10:51 +0300

    Add User Repository

--- src/main/java/telegrammrentalbot/rentbot/dto/UserDto.java
+++ src/main/java/telegrammrentalbot/rentbot/dto/UserDto.java
@@ -3,11 +3,13 @@ package telegrammrentalbot.rentbot.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.Id;
 
 @Getter
 @Setter
 @AllArgsConstructor
 public class UserDto {
+    @Id
     private long id;
     private String name;
     private boolean isBot;
