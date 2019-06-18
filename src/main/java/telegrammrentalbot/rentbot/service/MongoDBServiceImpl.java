e2343e86c1f126925599191b07a5a72bf5f12400
RomaE <ere.roma@gmail.com>
2019-06-18 13:13:00 +0300

    Add User Repository

--- src/main/java/telegrammrentalbot/rentbot/service/MongoDBServiceImpl.java
+++ src/main/java/telegrammrentalbot/rentbot/service/MongoDBServiceImpl.java
@@ -1,6 +1,7 @@
 package telegrammrentalbot.rentbot.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import telegrammrentalbot.rentbot.dto.RentObjectDto;
 import telegrammrentalbot.rentbot.repo.MongoDbRepo;
 
@@ -11,12 +12,14 @@ public class MongoDBServiceImpl implements IMongoDBService {
     MongoDbRepo mongoDbRepo;
 
     @Override
+    @Transactional
     public boolean createRent(RentObjectDto rentObjectDto) {
         mongoDbRepo.save(rentObjectDto);
         return true;
     }
 
     @Override
+    @Transactional
     public boolean delleteRent(Long id) {
         RentObjectDto rentObjectDto = mongoDbRepo.findById(id).orElse(null);
         if (rentObjectDto!=null) {
@@ -27,11 +30,13 @@ public class MongoDBServiceImpl implements IMongoDBService {
     }
 
     @Override
+    @Transactional
     public List<RentObjectDto> allAreaRents(String area) {
         return mongoDbRepo.findByArea(area);
     }
 
     @Override
+    @Transactional
     public List<RentObjectDto> allUserRents(long userId) {
         return mongoDbRepo.findByUserId(userId);
     }
